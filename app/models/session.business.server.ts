604cd32819366dfaf3fd46831e5c2699f1f543be
Harun Sokullu <harunsokullu@gmail.com>
2025-01-03 02:53:09 +0300

    Move session list to business

--- app/models/session.business.server.ts
+++ app/models/session.business.server.ts
@@ -3,3 +3,7 @@ import db from "../db.server";
 export function deleteSession(shop: string) {
   return db.session.deleteMany({ where: { shop } });
 }
+
+export function getAllSessions() {
+  return db.session.findMany();
+}
