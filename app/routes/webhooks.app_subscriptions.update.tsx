5d5c942a637a127fa822b62968e8d1268688f619
Harun Sokullu <harunsokullu@gmail.com>
2025-01-08 02:46:00 +0300

    Log subscription

--- app/routes/webhooks.app_subscriptions.update.tsx
+++ app/routes/webhooks.app_subscriptions.update.tsx
@@ -4,6 +4,7 @@ import { publish } from "../consumers/subscription-update.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { payload, shop } = await authenticate.webhook(request);
+  console.log(`Shop: ${shop}, Payload: ${JSON.stringify(payload)}`);
   const subscriptionName = payload.name as string;
   await publish(shop, subscriptionName);
   return new Response(null, { status: 200 });
